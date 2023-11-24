7d5ddc448d0dbf52749a0e0b1bb34bd9b5aa6a6c
RenSeven213 <foxyplay@bk.ru>
2023-11-24 20:56:39 +0700

    vue cli
    Поставил фреймворк vue cli

---
+++ hello-world/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div>
+            <button v-on:click="addLike">Like</button>
+        </div>
+        <div><strong>Кол-во лайков:</strong> {{ likes }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            likes: 0,
+            
+        }
+    },
+    methods: {
+        addLike() {
+            this.likes += 10000;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
